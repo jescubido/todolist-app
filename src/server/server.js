4983d02dff4421fb9333569b51fdd004b8e4a885
jescubido <jarisse.escubido@gmail.com>
2025-06-20 21:57:48 -0700

    separate frontend and backend server, update to have root package.json file

---
+++ src/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
